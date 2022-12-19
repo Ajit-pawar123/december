8df4ac770ef420da4751383858bcb11d0f996251
Ajit <Ajit@LAPTOP-K88R6NSI>
2022-12-19 17:44:22 +0530

    Hi LogInPage

---
+++ src/main/java/PageLayer/PageLayerClass.java
@@ -0,0 +1,10 @@
+package PageLayer;
+
+public class PageLayerClass {
+
+	
+	public void LogInPage()
+	{
+		System.out.println("user on log in page");
+	}
+}
---
+++ src/test/java/LoginPageTest/LoginFunctionality.java
@@ -0,0 +1,14 @@
+package LoginPageTest;
+
+import BaseLayer.BaseClass;
+
+public class LoginFunctionality extends BaseClass{
+	
+	
+  public static void main(String[] args) {
+	
+	  LoginFunctionality login=new LoginFunctionality();
+	  
+	  
+}
+}
